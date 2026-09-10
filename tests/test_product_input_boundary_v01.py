3f6cfa8d97840b052cc3f2a594e81eaa3e9b7176
Felipe Abal <felipe.c.abal@hotmail.com>
2026-09-10 15:41:36 -0300

    test: cover Product Input Boundary schema 0.3

--- tests/test_product_input_boundary_v01.py
+++ tests/test_product_input_boundary_v01.py
@@ -79,6 +79,21 @@ class ProductInputBoundaryRealFlowTests(unittest.TestCase):
         self.assertNotEqual(bundle.clean_package_bytes, pkg)
         self.assertNotEqual(bundle.review_package_bytes, bundle.clean_package_bytes)
 
+    def test_line_spacing_schema_03_e2e(self):
+        pkg = _pkg(
+            '<w:p><w:pPr><w:pStyle w:val="Normal"/>'
+            '<w:spacing w:line="480" w:lineRule="auto"/></w:pPr>'
+            '<w:r><w:rPr><w:sz w:val="24"/></w:rPr>'
+            '<w:t>spacing</w:t></w:r></w:p>'
+        )
+        profile_json = _json(
+            {"body": {"line_spacing": {"mode": "exact", "value": 1.5}}},
+            schema="0.3",
+        )
+        bundle = build_product_from_inputs(pkg, profile_json)
+        self.assertEqual(bundle.processing_report.summary.applied_change_count, 1)
+        self.assertNotEqual(bundle.clean_package_bytes, pkg)
+
     def test_alignment_schema_02_e2e(self):
         pkg = _pkg(
             '<w:p><w:pPr><w:jc w:val="left"/></w:pPr>'
